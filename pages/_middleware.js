fb01d70bcc8adedcd828e0f85160cb7b5dc63f0b
Sachin Bairi <31911680+hashiramauchiha@users.noreply.github.com>
2022-01-12 16:15:08 +0530

    Updated secureCookie

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,9 +3,17 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   // token will exist if user is logged in
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
+  // const token = await getToken({ req, secret: process.env.JWT_SECRET });
+   const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith('https://') ??
+      !!process.env.VERCEL_URL,
+  });
+  
   const { pathname } = req.nextUrl;
+  console.log(pathname);
 
   // allow the requests if the following is true...
   // 1] its a request for next-auth session and provider fetching
